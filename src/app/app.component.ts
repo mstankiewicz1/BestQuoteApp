0d1858f5806992a09c7f6d0e86764da30175ad90
Marek <marekstankiewicz6@wp.pl>
2023-01-31 17:37:18 +0100

    add basic func

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { QUOTES } from './models/data-base';
+import { Quotation } from './models/quotation';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  showForm = false;
+  quotes: Quotation[] = QUOTES; 
+  quotation: Quotation = { author: '', sentence: '', votes: 0 };
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = { author: '', sentence: '', votes: 0 };
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value; 
+  }
+
+}
